ee17db3e9e2f5055e98b616a81ea41a7426bac58
cmasuku <cmasuku@andrew.cmu.edu>
2020-04-03 10:34:37 -0400

    Added a hello.py file to run scrap code

---
+++ hello.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 11:26:52 2019
+
+@author: MasukuCM
+"""
+
+# New Scrap File for Trying out stuff
+
+# import sys
+
+
+# Define a main() function that prints a little greeting.
+def main():
+    print('Welcome to the practice doc!')
+#    print sys.argv
+    
+
+
+# Info from Google's Python Class
+# PyQuick - Basic Python
+
+
+# This is the standard boilerplate that calls the main() function
+if __name__ == '__main__':
+    main()
+
