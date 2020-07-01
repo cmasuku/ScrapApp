7659a9dce5a7f9d1ad836a685ce701f1aa228e3c
cmasuku <cmasuku@andrew.cmu.edu>
2020-07-01 11:15:46 -0400

    added a new file to try out new things

---
+++ giraffe.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Scrap Python file for trying out random code
+Created on Tue Jun 30 2020
+
+From watching Free Courses on YouTube
+By: freeCodeCamp.org
+Title: Learn Python -- Full Course for Beginners [Tutorial]
+ 
+@author: MasukuCM
+"""
+# Preferred Editor/IDE PyCharm
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = np.linspace(0.0, 5.0)
+x2 = np.linspace(0.0, 2.0)
+
+y1 = np.cos(2*np.pi*x1)
+y2 = np.cos(2*np.pi*x2)
+
+plt.subplot(2,1,1)
+plt.plot(x1, y1, 'o-')
+plt.title('A tale of 2 subplots')
+plt.ylabel('Damped oscillation')
+
+plt.subplot(2,1,2)
+plt.plot(x2, y2, '.-')
+plt.xlabel('time (s)')
+plt.ylabel('Undamped')
+
+# Unrelated code
+character_name = 'George'
+character_age = 35
+
