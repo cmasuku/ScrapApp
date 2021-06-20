a2321cdc9c6d1da1a7e2e1a1d93a10ae2a5dcdb1
Cornelius Masuku <44091752+cmasuku@users.noreply.github.com>
2021-06-20 12:26:11 -0400

    Update rosenbrock.py
    looping over initial values

--- rosenbrock.py
+++ rosenbrock.py
@@ -27,8 +27,19 @@ def rosenbrock(m):
     return (1.0 - m.x)**2 + 100.0*(m.y - m.x**2)**2
 model.obj = Objective(rule=rosenbrock, sense=minimize)
 
-solver = SolverFactory('ipopt')
-solver.solve(model, tee=True)
+print('x_init, y_init, x_soln, y_soln')
+y_init = 5.0
+for x_init in range(2, 6):
+    model.x = x_init
+    model.y = 5.0
+    
+    solver = SolverFactory('ipopt')
+    solver.solve(model)
+    
+    print("{0:6.2f} {1:6.2f} {2:6.2f} {3:6.2f}".format(x_init, y_init, value(model.x), value(model.y)))
+
+# solver = SolverFactory('ipopt')
+# solver.solve(model, tee=True)
 
 print()
 print('*** Solution ***:')
