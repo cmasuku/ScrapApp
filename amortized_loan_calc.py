d81704b28f95ef667915b3c4a404bdbbf90dee6d
cmasuku <cmasuku@pal-10-184-86-121.itap.purdue.edu>
2023-05-03 11:33:34 -0400

    added a loan calculation file for fun

---
+++ amortized_loan_calc.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def amortized_loan(P,i,N):
+    """
+    --Parameters--
+    P: Principal borrowed
+    i: interest rate
+    N: number of payments
+
+    --Return--
+    A DataFrame that contain principal/interest by month
+    """
+
+    summary{}
+
+    
